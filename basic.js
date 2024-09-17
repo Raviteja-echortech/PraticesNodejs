9d49f9af826ed8dae50b3e35ba77b124a8424e7a
Ravi teja <raviteja@echortech.com>
2024-09-17 11:07:08 +0530

    basic Nodejs code added

---
+++ basic.js
@@ -0,0 +1,2 @@
+const build = require("./index")
+console.log( build.add(9,10),build.Sub(10,3))
---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log("Hey there! I am JS")
+function add(a, b) {
+    return a + b
+}
+function Sub(a, b) {
+    return a - b
+}
+
+module.exports = { add, Sub }
